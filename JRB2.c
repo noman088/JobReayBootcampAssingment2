e9bc86e7786ef2a3bd4842adbd1bb79f77adc243
noman088 <109752092+noman088@users.noreply.github.com>
2022-08-17 12:37:46 +0530

    Update JRB2.c

--- JRB2.c
+++ JRB2.c
@@ -226,9 +226,9 @@
 
 // 12. Assume price of 1 USD is INR 76.23. Write a program to take the amount in INR and
 // convert it into USD.
-#include<stdio.h>
-#include<conio.h>
-int main()
+// #include<stdio.h>
+// #include<conio.h>
+// int main()
 // {
 //    int INR,inr;
 //    float USD,usd;
